7f9c6b610fececd3e72c37bad2540c68ac74f1e7
Tammy Truong <thtruong@usc.edu>
2022-04-14 10:11:51 -0700

    Fixed App.css formatting

--- src/App.js
+++ src/App.js
@@ -15,34 +15,38 @@ function App() {
       </header>
       <body class="container-fluid p-0 m-0">
         
-        {/* HEADER SECTION */}
-
-        <div class="heading-title pt-5 pb-3 bg-image d-flex align-items-center">
+        {/* ------------ HEADER SECTION ------------ */}
+        <div class="heading-title pt-5 pb-3 bg-image d-flex align-items-center justify-content-center">
           <div class="container-fluid">
-            <h3 class="d-flex pb-0 mb-0 container-fluid justify-content-center">USC Ukraine Assistance Hub</h3>
+            <h3 class="d-flex pb-0 mb-0 container-fluid justify-content-center"><b>USC Ukraine Assistance Hub</b></h3>
             <p class="d-flex pt-0 mt-0 container-fluid justify-content-center"><br/>
-              <b>If you are a faculty member, postdoc, graduate, or undergraduate student who has left or is 
+              <p>If you are a faculty member, postdoc, graduate, or undergraduate student who has left or is 
               considering leaving Ukraine because of the war, USC may offer you a place to continue your research, work or studies. Funding, visa support, accommodation, 
-              mentoring, and other resources are available.</b><br/><br/>
-
-              Please see <b>general pathways</b> as well as <b>specific opportunities</b> for students and scholars listed 
-              below. Even if you do not see an existing opening, please submit your information through the Interest Form below. We will strive to connect you with specific 
-              faculty and programs at USC.<br/><br/>
-
-              Over 350 USC-based faculty have expressed interest in supporting Ukrainian students and academics, and resources are continually being updated internally!<br/><br/>
-            
-              <b>For USC Faculty:</b> If you'd like to add your resources or collaboration interests, please use <a href="GOOGLEFORMLINK">this form</a>.<br/><br/>
-            
-              <i>This is a joint effort of USC faculty members with support and oversight by the Office of 
-              Strategic and Global Initiatives at USC. For additional information, you can contact our working group directly via <a href="ukraineassistance@usc.edu">ukraineassistance@usc.edu</a>.</i>
+              mentoring, and other resources are available.</p>
             </p>
           </div>
         </div>
 
-        {/* ------------ INTRODUCTION SECTION CODE ------------ */}
 
+        {/* ------------ INTRODUCTION SECTION CODE ------------ */}
         <div class="content container justify-content-center col-xs-6 mt-5 col-lg-7">
-         <h1 class="d-flex pb-0 mb-0 container-fluid justify-content-center"><b><u>General Pathways for Interested Scholars and Students</u></b></h1><br/>
+
+          Please see <b>general pathways</b> as well as <b>specific opportunities</b> for students and scholars listed 
+          below. Even if you do not see an existing opening, please submit your information through the Interest Form below. We will strive to connect you with specific 
+          faculty and programs at USC.
+
+          Over 350 USC-based faculty have expressed interest in supporting Ukrainian students and academics, and resources are continually being updated internally!<br/><br/>
+        
+          <b>For USC Faculty:</b> If you'd like to add your resources or collaboration interests, please use <a href="GOOGLEFORMLINK">this form</a>.<br/><br/>
+        
+          <i>This is a joint effort of USC faculty members with support and oversight by the Office of 
+          Strategic and Global Initiatives at USC. For additional information, you can contact our working group directly via <a href="ukraineassistance@usc.edu">ukraineassistance@usc.edu</a>.</i>
+        </div>
+        
+          
+        {/* ------------ GENERAL PATHWAYS SECTION ------------ */}
+        <div class="content container justify-content-center col-xs-6 mt-5 col-lg-7">
+         <h1 class="d-flex pb-0 mb-0 container-fluid justify-content-center"><b>General Pathways for Interested Scholars and Students</b></h1><br/>
           <p1 class="text-align-center">
             <b>Undergraduate Programs:</b> leaving this empty before we know what we can offer. 
             To learn more about undergraduate education at USC, please click <a href="https://admission.usc.edu/">here</a>.<br/><br/>
@@ -60,9 +64,6 @@ function App() {
             other resources are available. To learn more, please fill out the <b><u><a href="INTERESTFORMLINK">interest form</a></u></b> here. Please fill the form out even if you apply directly via <a href="https://usccareers.usc.edu/">USC Careers</a>.
           </p1>
         </div> 
-          
-        {/* ------------ GENERAL PATHWAYS SECTION ------------ */}
-
 
 
 
@@ -76,8 +77,8 @@ function App() {
 
         {/* ------------ NON-USC OPPORTUNITIES SECTION ------------ */}
 
-        </div>
       </body>
+
       <footer class="footer container-fluid static-top">
       </footer>
     </div>

0995c897482e346eca072aa3ba457e60e091c261
genia-dru <86218340+genia-dru@users.noreply.github.com>
2022-04-14 08:28:06 -0700

    Included Header/Introduction

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import './App.css';
 import 'react-dropdown/style.css';
 import InterestForm from './interestForm.js';
 
-
 function App() {
   return (
     <div className="App">
@@ -14,25 +13,53 @@ function App() {
             <img src={logo} class="logo d-none d-sm-block justify-content-right"/>     
       </div>
       </header>
-    
       <body class="container-fluid p-0 m-0">
-
+        
         {/* HEADER SECTION */}
+
         <div class="heading-title pt-5 pb-3 bg-image d-flex align-items-center">
           <div class="container-fluid">
             <h3 class="d-flex pb-0 mb-0 container-fluid justify-content-center">USC Ukraine Assistance Hub</h3>
-            <p class="d-flex  pt-0 mt-0 container-fluid justify-content-center">USC faculty offer support for scholars and students displaced by the war in Ukraine</p>
+            <p class="d-flex pt-0 mt-0 container-fluid justify-content-center"><br/>
+              <b>If you are a faculty member, postdoc, graduate, or undergraduate student who has left or is 
+              considering leaving Ukraine because of the war, USC may offer you a place to continue your research, work or studies. Funding, visa support, accommodation, 
+              mentoring, and other resources are available.</b><br/><br/>
+
+              Please see <b>general pathways</b> as well as <b>specific opportunities</b> for students and scholars listed 
+              below. Even if you do not see an existing opening, please submit your information through the Interest Form below. We will strive to connect you with specific 
+              faculty and programs at USC.<br/><br/>
+
+              Over 350 USC-based faculty have expressed interest in supporting Ukrainian students and academics, and resources are continually being updated internally!<br/><br/>
+            
+              <b>For USC Faculty:</b> If you'd like to add your resources or collaboration interests, please use <a href="GOOGLEFORMLINK">this form</a>.<br/><br/>
+            
+              <i>This is a joint effort of USC faculty members with support and oversight by the Office of 
+              Strategic and Global Initiatives at USC. For additional information, you can contact our working group directly via <a href="ukraineassistance@usc.edu">ukraineassistance@usc.edu</a>.</i>
+            </p>
           </div>
         </div>
 
-        <div class="content container justify-content-center col-xs-6 mt-5 col-lg-7">
         {/* ------------ INTRODUCTION SECTION CODE ------------ */}
 
-          <p1 class="text-align-center">If you are a faculty member or a graduate student who left or is considering leaving Ukraine because of the war, USC 
-          may offer you a temporary place to continue your work or studies. Funding and visa support are available. See below 
-          for the available opportunities in each USC school. Please contact <a href="email@usc.edu">email@usc.edu</a> if you are interested or if you have
-          any questions.</p1>
-          
+        <div class="content container justify-content-center col-xs-6 mt-5 col-lg-7">
+         <h1 class="d-flex pb-0 mb-0 container-fluid justify-content-center"><b><u>General Pathways for Interested Scholars and Students</u></b></h1><br/>
+          <p1 class="text-align-center">
+            <b>Undergraduate Programs:</b> leaving this empty before we know what we can offer. 
+            To learn more about undergraduate education at USC, please click <a href="https://admission.usc.edu/">here</a>.<br/><br/>
+
+            <b>Master's Programs:</b> We may be able to offer you to continue your studies at 
+            USC while the war lasts. Funding, visa support, accommodation, mentoring, and other resources are available. To learn more, please fill out 
+            the <b><u><a href="INTERESTFORMLINK">interest form</a></u></b> here. You may review a list of all master's programs at USC <a href="https://gradadm.usc.edu/our-programs/">here</a>.<br/><br/>
+            
+            <b>Doctoral Programs:</b> We may be able to offer you to continue your studies and 
+            research at USC. Lab space and placement in specific research teams may be available. Funding, visa support, accommodation, mentoring, and other 
+            resources are available. To learn more, please fill out the <b><u><a href="INTERESTFORMLINK">interest form</a></u></b> here. You may review a list of all doctoral programs at USC <a href="https://gradadm.usc.edu/our-programs/">here</a>.<br/><br/>
+            
+            <b>Postdoctoral and Visiting Faculty Opportunities:</b> We may be able to offer you to 
+            continue your work or research at USC. Research and teaching opportunities may be available. Funding, visa support, accommodation, mentoring, and 
+            other resources are available. To learn more, please fill out the <b><u><a href="INTERESTFORMLINK">interest form</a></u></b> here. Please fill the form out even if you apply directly via <a href="https://usccareers.usc.edu/">USC Careers</a>.
+          </p1>
+        </div> 
           
         {/* ------------ GENERAL PATHWAYS SECTION ------------ */}
 
@@ -44,17 +71,13 @@ function App() {
 
 
         {/* ------------ INTEREST FORM SECTION ------------ */}
+
           <InterestForm></InterestForm>
 
         {/* ------------ NON-USC OPPORTUNITIES SECTION ------------ */}
 
-
-
         </div>
-
       </body>
-
-
       <footer class="footer container-fluid static-top">
       </footer>
     </div>
